7fb738b497870e754cce0579cc9a2eaefdde62a9
Ayazhan <kuanysh.ayazhan070503@gmail.com>
2021-10-09 16:49:54 +0600

    lab 4

--- lab3/task2.sql
+++ lab3/task2.sql
@@ -3,6 +3,20 @@ select dept_name, avg(salary)
 from instructor
 group by dept_name
 order by avg(salary);
+--b
+select building, count(building)
+from section
+group by building
+order by count(building) desc
+fetch first 1 rows only;
+--c
+select dept_name, count(dept_name)
+from course
+group by dept_name
+order by count(dept_name)
+fetch first 1 rows only;
+--d
+
 --e
 select * from instructor
 where dept_name = 'Biology' or dept_name = 'Philosophy' or dept_name = 'Music';
--- lab3/task3.sql
+++ lab3/task3.sql
@@ -1,10 +1,13 @@
 --a
-select name from takes,student
-where takes.id = student.id and  dept_name = 'Comp.Sci' and (grade = 'A' or grade = 'A-')
+select name
+from takes,student
+where takes.id = student.id and dept_name = 'Comp.Sci' and (grade = 'A' or grade = 'A-')
 order by name;
 --b
-select id, grade from takes
+select id, grade
+from takes
 where grade <> 'B' and grade <> 'A' and grade <> 'A-' and grade <> 'B+';
 --c
-select id, grade from takes
+select id, grade
+from takes
 where grade <> 'F' and grade <> 'C';
